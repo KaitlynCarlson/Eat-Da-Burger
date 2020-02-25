b883ad8c5a9d9d5014899b86a2077eae0a4076a9
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-02-24 18:17:39 -0800

    Populated burgers_db with burger_name data

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES("Pepper Jack 'n Bacon Burger");
+INSERT INTO burgers
+    (burger_name)
+VALUES("Classic American Cheese 'n Chill Burger");
+INSERT INTO burgers
+    (burger_name)
+VALUES("Mushroom Monster Mash Burger");
+INSERT INTO burgers
+    (burger_name)
+VALUES("Avo-Ipster Pepper-O-Burger");
+INSERT INTO burgers
+    (burger_name)
+VALUES("Wild Onion Bill BBQ Burger");
+INSERT INTO burgers
+    (burger_name)
+VALUES("Double Grilled Cheese Double Patty Belly ");
+SELECT *
+FROM burgers;
