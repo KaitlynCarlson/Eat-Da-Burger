34003816e7b8259eb5807188ab0a3da664b21b00
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-02-25 00:24:27 -0800

    Created burger.js calling ORM functions

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,22 @@
 "use strict";
 
 const orm = require("../config/orm");
+const burger = {
+  selectAll: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  createOne: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+module.exports = burger;
