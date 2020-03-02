2ea3bfdc88b571faa32c7bb50c8c1bc98dded3d7
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-01 19:44:20 -0800

    Trying to connect SQL to heroku

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@
 const mysql = require("mysql");
 
 let connection;
+
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -14,7 +15,12 @@ if (process.env.JAWSDB_URL) {
     database: "burgers_db"
   });
 }
-
-connection.connect();
+connection.connect(err => {
+  if (err) {
+    console.error("error connecting", err.stack);
+    return;
+  }
+  console.log(`connected with id ${connection.threadId}`);
+});
 
 module.exports = connection;
